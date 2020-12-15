0fac8ade5009c2fb716aa4d4be6865a7aaa25417
Yann Klein <yann.klein@me.com>
2020-12-15 13:06:10 +0900

    adapt sushi-ai to built a simple to use starlette boilerplate

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
-# Starter for deploying [fast.ai](https://www.fast.ai) models on [Render](https://render.com)
+# Starlette + FastAI boilerplate
+A simple boilerplate of [Starlette](https://www.starlette.io/) webapp running a AI model that recognizes Sushis.
+The model is built using [FastAI library](https://github.com/fastai/fastai)
 
-This repo can be used as a starting point to deploy [fast.ai](https://github.com/fastai/fastai) models on Render.
+# Getting started
 
-The sample app described here is up at https://fastai-v3.onrender.com. Test it out with bear images!
-
-You can test your changes locally by installing Docker and using the following command:
+Clone the rep on your machine:
 
 ```
-docker build -t fastai-v3 . && docker run --rm -it -p 5000:5000 fastai-v3
+git clone git@github.com:yannklein/starlette-fastai-boilerplate.git
 ```
 
-The guide for production deployment to Render is at https://course.fast.ai/deployment_render.html.
+Install the project packages:
+```
+pipenv install
+```
 
-Please use [Render's fast.ai forum thread](https://forums.fast.ai/t/deployment-platform-render/33953) for questions and support.
+Run the webapp on a local server:
+```
+python app/server.py serve
+```
--- app/server.py
+++ app/server.py
@@ -1,7 +1,9 @@
 import aiohttp
 import asyncio
 import uvicorn
+import json
 from fastai import *
+from fastai.imports import *
 from fastai.vision import *
 from io import BytesIO
 from starlette.applications import Starlette
@@ -9,86 +11,30 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-# Base example model
-# export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-# export_file_name = 'export.pkl'
-
-# https://www.dropbox.com/s/oeha2t6ip0w2thl/super-multi-sushi.pth?dl=0
-# https://www.dropbox.com/s/k9wqc1p5lkgrav8/clean-super-multi-sushi.pth?dl=0
-
-# Custom model
-model_name = "clean-super-multi-sushi"
-export_file_url = f'https://www.dropbox.com/s/k9wqc1p5lkgrav8/{model_name}.pth?raw=1'
-model_file_name = 'model'
-export_file_name = f'models/{model_file_name}.pth'
-
+# Project root path
 path = Path(__file__).parent
 
-# Base example classes
-# classes = ['black', 'grizzly', 'teddys']
+# Model file path
+model_filepath = "models/model.pth"
 
-# Custom classes
-# classes = ['salmon','tuna','saba','aji','anago','unagi','tamago','ikura','ebi','tai']
-classes = [
-  {'jp': 'サーモン', 'en': 'salmon', 'url': "http://www.sushiencyclopedia.com/sushi-fish/salmon.html"},
-  {'jp': 'マグロ', 'en': 'tuna', 'url': "http://www.sushiencyclopedia.com/sushi-fish/tuna.html"},
-  {'jp': 'さば', 'en': 'mackerel', 'url': "http://www.sushiencyclopedia.com/sushi-fish/mackerel.html"},
-  {'jp': 'アジ', 'en': 'spanish mackerel', 'url': "http://www.sushiencyclopedia.com/sushi-fish/spanish_mackerel.html"},
-  {'jp': 'アナゴ', 'en': 'sea eel', 'url': "http://www.sushiencyclopedia.com/sushi-fish/sea-eel.html"},
-  {'jp': 'ウナギ', 'en': 'eel', 'url': "http://www.sushiencyclopedia.com/sushi-fish/eel.html"},
-  {'jp': '卵', 'en': 'egg', 'url': "http://www.sushiencyclopedia.com/sushi-menu/egg_sushi.html"},
-  {'jp': 'いくら', 'en': 'salmon roe', 'url': "http://www.sushiencyclopedia.com/sushi-menu/salmon_roe_sushi.html"},
-  {'jp': 'えび', 'en': 'shrimp', 'url': "http://www.sushiencyclopedia.com/sushi-menu/shrimp_sushi.html"},
-  {'jp': '鯛', 'en': 'sea bream', 'url': "http://www.sushiencyclopedia.com/sushi-fish/red_snapper.html"},
-  {'jp': 'つぶ貝', 'en': 'whelk', 'url': "https://en.wikipedia.org/wiki/Whelk"},
-  {'jp': 'ブリ', 'en': 'yellowtail fish', 'url': "http://www.sushiencyclopedia.com/sushi-fish/yellowtail-amberjack.html"},
-  {'jp': 'ホッキ貝', 'en': 'surf clam', 'url': "https://www.sushifaq.com/sushi-sashimi-info/sushi-item-profiles/akagai-surf-clam/"},
-  {'jp': '縁側', 'en': 'halibut fin', 'url': "http://www.sushiencyclopedia.com/sushi-fish/halibut.html"},
-  {'jp': 'たこ', 'en': 'octopus', 'url': "http://www.sushiencyclopedia.com/sushi-other-seafood/octopus.html"},
-  {'jp': 'イカ', 'en': 'squid', 'url': "http://www.sushiencyclopedia.com/sushi-menu/squid-sushi.html"},
-  {'jp': 'カンパチ', 'en': 'amberjack', 'url': "http://www.sushiencyclopedia.com/sushi-fish/amberjack.html"},
-  {'jp': 'イワシ', 'en': 'sardine', 'url': "http://www.sushiencyclopedia.com/sushi-fish/sardine.html"},
-  {'jp': 'ウニ', 'en': 'sea urchin', 'url': "https://www.sushifaq.com/sushi-sashimi-info/sushi-item-profiles/sushi-items-uni-sea-urchin/"},
-  {'jp': 'ホタテ', 'en': 'scallop', 'url': "https://www.justonecookbook.com/scallop-sushi/"},
-  {'jp': '赤貝', 'en': 'ark clam', 'url': "http://www.sushiencyclopedia.com/sushi-shellfish/ark-shell-clam.html"},
-  {'jp': 'かに', 'en': 'crab', 'url': "https://www.sushifaq.com/sushi-sashimi-info/sushi-item-profiles/kanikama-or-surimi/"},
-  {'jp': 'カツオ', 'en': 'bonito', 'url': "http://www.sushiencyclopedia.com/sushi-fish/bonito.html"},
-  {'jp': '小肌', 'en': 'shad', 'url': "http://www.sushiencyclopedia.com/sushi-menu/gizzard_shad_sushi.html"},
-  {'jp': '大トロ', 'en': 'fatty tuna', 'url': "http://www.sushiencyclopedia.com/sushi-menu/toro-tuna-belly-sushi.html"}
-]
+# Load classes info from JSON
+with open(path / 'classes/sushi.json') as json_file:
+    classes = json.load(json_file)
 classes = map(lambda classe: classe['en'], classes)
 classes = sorted(classes)
-print(classes)
 
-if os.path.isfile(export_file_name):
-  os.remove(export_file_name)
-  print("Old model removed!")
-
+# Initialize Starlette app
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
 app.mount('/static', StaticFiles(directory='app/static'))
 
-
-async def download_file(url, dest):
-    if dest.exists(): return
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.read()
-            with open(dest, 'wb') as f:
-                f.write(data)
-
-
+# Learning setup (dependent of your library!)
 async def setup_learner():
-    await download_file(export_file_url, path / export_file_name)
     try:
-        # Base code to use .pkl
-        # learn = load_learner(path, export_file_name)
-
-        # Custom code for .pth
-        data_bunch = ImageDataBunch.single_from_classes(path, classes,
-        ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
+        print("Model learning setup")
+        data_bunch = ImageDataBunch.single_from_classes(path, classes, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
         learn = cnn_learner(data_bunch, models.resnet34, pretrained=False)
-        learn.load(model_file_name)
+        learn.load("model")
 
         return learn
     except RuntimeError as e:
@@ -99,39 +45,45 @@ async def setup_learner():
         else:
             raise
 
-
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
 
-
+# Route to the homepage
 @app.route('/')
 async def homepage(request):
-    html_file = path / 'view' / 'index.html'
+    html_file = path / 'view/index.html'
     return HTMLResponse(html_file.open().read())
 
-
+# Route to the analyzer
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
+
+    # Get the uploaded image and store it in img
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
+
+    # Predict the image classe according to the model (dependent of your library!)
     prediction = learn.predict(img)
-    print('prediction:', round(prediction[2][prediction[1].item()].item() * 100))
+    print('prediction:', f'{str(prediction[0])} with {round(prediction[2][prediction[1].item()].item() * 100)}%')
     details = {}
     for index, each_class in enumerate(classes):
-      details[each_class] = round(prediction[2][index].item() * 100)
+        details[each_class] = round(prediction[2][index].item() * 100)
+    print('prediction details:',details)
 
-    return JSONResponse(
-      {
+    # Build the json file with analysis results
+    json_response = {
         'result': str(prediction[0]),
         'resultPct': round(prediction[2][prediction[1].item()].item() * 100),
         'details': details
-      }
-    )
+    }
+    print(json_response)
 
+    return JSONResponse(json_response)
 
+# Initialize the starlette web app when python 'app/server.py serve' is run
 if __name__ == '__main__':
     if 'serve' in sys.argv:
         uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
